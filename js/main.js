7df3b581bc0fe85a79ac5c3fa385496705be8932
Michael Larbi <mickylarbi28@gmail.com>
2025-08-05 17:49:11 +0000

    code challenge

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-console.log(Math.PI)
+// first challenge 
+// write code that will return a random letter from your name
 
-console.log(Math.trunc(Math.PI))
-console.log(Math.round(Math.PI))
-console.log(Math.ceil(Math.PI))
-console.log(Math.floor(Math.PI))
-
-console.log(Math.pow(2, 4))
-
-console.log(Math.min(2, 4, 1, 5, 2, 5))
-console.log(Math.max(2, 4, 1, 5, 2, 5))
-
-console.log(Math.random())
-console.log(Math.floor(Math.random() * 10) + 1) // generate random numbers from 1 to 10
+const myName = 'Michael'
+const index = Math.floor(Math.random() * myName.length)
+console.log(myName.charAt(index))
