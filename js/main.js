c3deb28ef67a1b82e49f8e9626bf770d7fe7ee60
Michael Larbi <mickylarbi28@gmail.com>
2025-08-05 18:17:46 +0000

    test grading example

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-let customerIsBanned = true;
+let testScore = 89
+let collegeStudent = true;
+let grade
 
-let soup = 'chicken noodle soup'
-let crackers = true
-let reply;
-
-if (customerIsBanned) {
-    reply = 'No soup for you!';
-} else if (soup && crackers) {
-    reply = `Here is your order of ${soup} and crackers.`
-} else if (soup) {
-    reply = `Here is your order of ${soup}.`
+if (testScore >= 90) {
+    grade = 'A';
+} else if (testScore >= 80) {
+    grade = 'B';
+} else if (testScore >= 70) {
+    grade = 'C';
+} else if (testScore >= 60) {
+    grade = 'D';
 } else {
-    reply = `Sorry we're out of ${soup}`
+    if (collegeStudent) {
+        grade = "U"
+    } else {
+        grade = 'F';
+    }
 }
 
-console.log(reply)
+console.log(grade)
