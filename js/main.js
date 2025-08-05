083a90d65505c9395b690858d08d56da104daf08
Michael Larbi <mickylarbi28@gmail.com>
2025-08-05 18:11:56 +0000

    soup conditions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-// first challenge 
-// write code that will return a random letter from your name
+let customerIsBanned = true;
 
-const myName = 'Michael'
-const index = Math.floor(Math.random() * myName.length)
-console.log(myName.charAt(index))
+let soup = 'chicken noodle soup'
+let crackers = true
+let reply;
+
+if (customerIsBanned) {
+    reply = 'No soup for you!';
+} else if (soup && crackers) {
+    reply = `Here is your order of ${soup} and crackers.`
+} else if (soup) {
+    reply = `Here is your order of ${soup}.`
+} else {
+    reply = `Sorry we're out of ${soup}`
+}
+
+console.log(reply)
