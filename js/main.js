1cb28e23b1a738a614c2a70b1eb83d0980ab884d
Michael Larbi <mickylarbi28@gmail.com>
2025-08-05 17:24:51 +0000

    strings and string methods

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-console.log('hello world');
-console.log(typeof "Dave");
-console.log(typeof 42);
-console.log(typeof true);
+const myVariable = 'Mathematics';
+
+// string methods
+console.log(myVariable.length)
+
+console.log(myVariable.charAt(0))
+
+
+console.log(myVariable.indexOf('M'))
+console.log(myVariable.indexOf('ath'))
+console.log(myVariable.lastIndexOf('at'))
+
+console.log(myVariable.slice(3, 7)); // returns characters starting from first argument to second argument - 
+
+console.log(myVariable.toUpperCase())
+console.log(myVariable.toLowerCase())
+
+console.log(myVariable.includes('ath'))
+
+console.log(myVariable.split('')) // returns an array of individual letters
+console.log(myVariable.split('e')) // splits the string at the character provided (array returned does not include character)
+
+
+
