e3150ab05e625b1b596896f8f3e934bae4a74da0
Michael Larbi <mickylarbi28@gmail.com>
2025-08-05 19:37:53 +0000

    prompt dialog

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-// confirmation dialog
-let myBoolean = confirm("ok === true\ncancel === false")
-console.log(myBoolean)
-
-
+// prompt dialog
+let myName = prompt('please enter your name');
+if (myName) {
+    console.log(myName.length)
+    console.log(myName.trim().length)
+    console.log(myName.trim())
+}
+else {
+    console.log('you did not enter your name')
+}
