1b7db03077ab76b459384c8560aa00c8225d9465
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 05:48:19 +0000

    rock paper scissors pt5

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
+let playGame = confirm('shall we play rock, paper or scissors?')
 
-const equipShelfA = ['baseball', 'football', 'volleyball']
-const equipShelfB = ['basketball', 'golf ball', 'tennis ball']
+if (playGame) {
+    let playerChoice = prompt('please enter rock, paper or scissors')
+    if (playerChoice) {
+        let player = playerChoice.trim().toLowerCase()
+        const rpsArray = ['rock', 'paper', 'scissors']
 
-const clothesShelfA = ['tank tops', 't-shirts', 'jerseys']
-const clothesShelfB = ['sweat tops', 'sweat pants', 'hoodies']
+        if (rpsArray.includes(player)) {
+            let computerChoice = Math.floor(Math.random() * 3)
+            let computer = rpsArray[computerChoice]
 
-console.log(equipShelfA[1])
-console.log(equipShelfB[0])
+            let result = `player: ${player}\ncomputer: ${computer}\n\n`;
 
-const equipDept = [equipShelfA, equipShelfB]
-const clothesDept = [clothesShelfA, clothesShelfB]
+            if (computer === player) {
+                result += 'tie game!'
+            } else if (rpsArray.indexOf(player) === 2 && rpsArray.indexOf(computer) === 0) {
+                result += 'computer wins'
+            } else if (rpsArray.indexOf(computer) === 2 && rpsArray.indexOf(player) === 0) {
+                result += 'player wins'
+            } else if (rpsArray.indexOf(player) > rpsArray.indexOf(computer)) {
+                result += 'player wins'
+            } else {
+                result += 'computer wins'
+            }
 
-console.log(equipDept[0][1])
-console.log(clothesDept[1][0])
+            alert(result)
 
-const sportsStore = [equipDept, clothesDept]
-console.log(sportsStore[0][1][0])
-console.log(sportsStore[1][0][1])
+            let playAgain = confirm('Shall we play again?')
+            playAgain ? location.reload() : alert('ok, thanks for playing')
+
+        } else {
+            alert('you did not enter rock, paper or scissors')
+        }
+
+    } else {
+        alert('i guess you changed your mind. maybe next time')
+    }
+} else {
+    alert('okay maybe next time')
+}
