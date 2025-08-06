ae968febc662a77244b55076be691edd9373ea35
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 19:56:11 +0000

    DOM

--- js/main.js
+++ js/main.js
@@ -1,78 +1,97 @@
-"use strict";
+const view1 = document.getElementById('view1')
+console.log(view1)
+const view2 = document.querySelector('#view2')
+console.log(view2)
 
-// const makeError = () => {
-//     try {
-//         const name = 'dave';
-//         name = ''
-//     } catch (error) {
-//         console.log(error)
-//         console.warn(error)
-//         console.error(error.stack)
-//     }
-// }
+view1.style.display = "flex"
+view2.style.display = "flex"
 
-// makeError()
+const views = document.getElementsByClassName('view')
+console.log(views)
+const sameViews = document.querySelectorAll('.view')
+console.log(sameViews)
 
+const divs = view1.querySelectorAll('div')
+console.log(divs)
+const sameDivs = view1.getElementsByTagName('div')
+console.log(sameDivs)
 
-//---------------------------------------------------------
-
-
-
-// const makeError = () => {
-//     try {
-//        throw new customError('something went wrong')
-//     } catch (error) {
-//         console.log(error)
-//         console.warn(error)
-//         console.error(error.stack)
-//     }
-// }
-
-// makeError()
+const evenDivs = view1.querySelectorAll('div:nth-of-type(2n)')
+console.log(evenDivs)
 
 
-// function customError(message) {
-//     this.message = message;
-//     this.name = "dave";
-//     this.stack = `${this.name} ${this.message}`
-// }
-
-//---------------------------------------------------------
+//------------------------------------------------------------
 
 
-
-// const makeError = () => {
-//     try {
-//        throw new Error('something went wrong')
-//     } catch (error) {
-//         console.log(error)
-//         console.warn(error)
-//         console.error(error.stack)
-//     } finally {
-//         console.log('do something')
-//     }
-// }
-// makeError()
-
-
-//---------------------------------------------------------
-
-
-
-const makeError = () => {
-    let i = 0
-    while (i <= 5) {
-        try {
-            if (i % 2 !== 0) {
-                throw new Error('Odd number')
-            }
-            console.log('Even number!')
-        } catch (error) {
-            console.error(error.stack)
-        } finally {
-            console.log('...finally')
-            i++
-        }
-    }
+for (const div of evenDivs) {
+    div.style.backgroundColor = 'darkblue'
+    div.style.width = '200px'
+    div.style.height = '200px'
 }
-makeError()
+
+
+//------------------------------------------------------------
+
+
+const navText = document.querySelector('nav h1')
+console.log(navText)
+navText.textContent = 'Hello World'
+const navbar = document.querySelector('nav')
+navbar.innerHTML = `<h1>Hello</h1> <p>This should align right</p>`
+console.log(navbar)
+navbar.style.justifyContent = 'space-between'
+
+
+//------------------------------------------------------------
+
+
+console.log(evenDivs[0])
+console.log(evenDivs[0].parentElement)
+console.log(evenDivs[0].parentElement.children)
+console.log(evenDivs[0].parentElement.childNodes)
+console.log(evenDivs[0].parentElement.hasChildNodes())
+console.log(evenDivs[0].parentElement.lastChild)
+console.log(evenDivs[0].parentElement.lastElementChild)
+console.log(evenDivs[0].parentElement.firstChild)
+console.log(evenDivs[0].parentElement.firstElementChild)
+console.log(evenDivs[0].nextSibling)
+console.log(evenDivs[0].nextElementSibling)
+console.log(evenDivs[0].previousSibling)
+console.log(evenDivs[0].previousElementSibling);
+
+
+//------------------------------------------------------------
+
+
+view1.style.display = "none";
+view2.style.display = "flex";
+view2.style.flexDirection = "row";
+view2.style.flexWrap = "wrap";
+view2.style.margin = "10px";
+
+
+//------------------------------------------------------------
+
+
+while (view2.lastChild) {
+    view2.lastChild.remove()
+}
+
+const createDivs = (parent, iter) => {
+    const newDiv = document.createElement('div');
+    newDiv.textContent = iter;
+    newDiv.style.backgroundColor = 'black'
+    newDiv.style.width = '100px'
+    newDiv.style.height = '100px'
+    newDiv.style.margin = '10px'
+    newDiv.style.display = 'flex'
+    newDiv.style.justifyContent = 'center'
+    newDiv.style.alignItems = 'center'
+    parent.append(newDiv)
+}
+
+
+for (let i = 1; i <= 12; i++) {
+    createDivs(view2, i)
+
+}
