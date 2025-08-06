c9af940638edf507fb484259d2243bbb9d33dd3f
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 04:43:26 +0000

    functions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-let myNumber = 0
 
-// while loop
-// while (myNumber < 50) {
-//     myNumber++
-//     console.log(myNumber)
-// }
-
-// do while loop (will execute at least once)
-// do {
-//     myNumber++
-//     console.log(myNumber)
-// } while (myNumber < 50)
-
-// for loop
-// let i = 0
-// for (; i < 10;) {
-//     i++
-// }
-for (let i = 0; i < 10; i++) {
-    console.log(i)
+function logSomethingToConsole() {
+    console.log('something')
 }
 
+function logToConsole(something) {
+    console.log(something)
+}
+
+function addNumbers(num1, num2) {
+    return num1 + num2
+}
+
+const getUsernameFromEmail = function (email) {
+    return email.slice(0, email.indexOf('@'));
+}
+
+const toProperCase = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+}
+
+logSomethingToConsole()
+logToConsole('logged to console')
+
+console.log(addNumbers(2, 3))
+console.log(getUsernameFromEmail('kwame@dev.start'))
+console.log(toProperCase('dAvE'))
