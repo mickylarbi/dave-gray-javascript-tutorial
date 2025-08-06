34d9c9611e60c8320e4d85ba20cd131650bf05e0
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 05:20:35 +0000

    arrays (single dimensional)

--- js/main.js
+++ js/main.js
@@ -1,30 +1,61 @@
-// // can redeclare variable
-// var x = 1;
-// var x = 2;
+const myArray = []
 
-// // cannot redeclare but can reassign
-// let y = 1;
-// y = 2;
+// add elements to an array
+myArray[0] = "Dave"
+myArray[1] = 1001
+myArray[2] = false
 
-// // can neither reassign nor redeclare
-// const z = 3;
+console.log(myArray)
 
-
-
-// global scope
-var x = 1;
-let y = 2;
-const z = 3;
+console.log(myArray.length)
 
 
-// local scope (inside functions, loops, conditions or just curly brackets)
-{
-    let y = 3;
-    const z = 2;
-}
+// get element by index
+console.log(myArray[0])
 
-// local scope
-function myFunc() {
-    let y = 3;
-    const z = 2;
-}
+// last element
+console.log(myArray[myArray.length - 1])
+
+
+// array methods
+myArray.push('school')
+console.log(myArray)
+
+const lastItem = myArray.pop()
+console.log(myArray)
+console.log(lastItem)
+
+// adds to the beginning of the array
+myArray.unshift(42)
+console.log(myArray)
+
+// removes first element of the array
+myArray.shift()
+console.log(myArray)
+
+// array.splice(<starting index>, <number of elements to remove(optional)>, <insert at index(optional)>)
+myArray.splice(1, 0, 42)
+console.log(myArray)
+
+// array.slice(<starting index>, <ending index(not included)>)
+const newArray = myArray.slice(1)
+console.log(myArray)
+console.log(newArray)
+
+myArray.reverse()
+console.log(myArray)
+
+const joinedArray = myArray.join()
+console.log(myArray)
+console.log(joinedArray)
+
+const splitArray = joinedArray.split(',')
+console.log(splitArray)
+
+const concatArray = splitArray.concat(newArray)
+console.log(concatArray)
+
+const spreadArray = [...splitArray, ...newArray]
+console.log(spreadArray)
+
+
