2dc8ee8a715514836ea9af68d2f96962513cdc28
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 04:32:29 +0000

    loops

--- js/main.js
+++ js/main.js
@@ -1,36 +1,23 @@
-let playGame = confirm('shall we play rock, paper or scissors?')
+let myNumber = 0
 
-if (playGame) {
-    let playerChoice = prompt('please enter rock, paper or scissors')
-    if (playerChoice) {
-        let player = playerChoice.trim().toLowerCase()
+// while loop
+// while (myNumber < 50) {
+//     myNumber++
+//     console.log(myNumber)
+// }
 
-        if (player === 'rock' || player === 'paper' || player === 'scissors') {
-            let computerChoice = Math.floor(Math.random() * 3) + 1
-            let computer = computerChoice === 1
-                ? 'rock' : computerChoice === 2
-                    ? 'paper' : 'scissors'
+// do while loop (will execute at least once)
+// do {
+//     myNumber++
+//     console.log(myNumber)
+// } while (myNumber < 50)
 
-            let result = `player: ${player}\ncomputer: ${computer}\n\n`;
-
-            result = result + (player === computer ? 'player one:  tie game!' :
-                player === 'rock' && computer === 'paper' ? 'computer wins' :
-                    player === 'paper' && computer === 'scissors' ? 'computer wins' :
-                        player === 'scissors' && computer === 'rock' ? 'computer wins' :
-                            'player wins!');
-
-            alert(result)
-
-            let playAgain = confirm('Shall we play again?')
-            playAgain ? location.reload() : alert('ok, thanks for playing')
-
-        } else {
-            console.log('you did not enter rock, paper or scissors')
-        }
-
-    } else {
-        alert('i guess you changed your mind. maybe next time')
-    }
-} else {
-    alert('okay maybe next time')
+// for loop
+// let i = 0
+// for (; i < 10;) {
+//     i++
+// }
+for (let i = 0; i < 10; i++) {
+    console.log(i)
 }
+
