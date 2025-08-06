8467a547875bdb27315b04ca3eaa4c2d18dffaf7
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 16:29:43 +0000

    error handling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,78 @@
+"use strict";
 
-const myObj = {
-    name: 'Dave',
-    hobbies: ['eat', 'sleep', 'code'],
-    hello: function () { console.log('hello') }
+// const makeError = () => {
+//     try {
+//         const name = 'dave';
+//         name = ''
+//     } catch (error) {
+//         console.log(error)
+//         console.warn(error)
+//         console.error(error.stack)
+//     }
+// }
+
+// makeError()
+
+
+//---------------------------------------------------------
+
+
+
+// const makeError = () => {
+//     try {
+//        throw new customError('something went wrong')
+//     } catch (error) {
+//         console.log(error)
+//         console.warn(error)
+//         console.error(error.stack)
+//     }
+// }
+
+// makeError()
+
+
+// function customError(message) {
+//     this.message = message;
+//     this.name = "dave";
+//     this.stack = `${this.name} ${this.message}`
+// }
+
+//---------------------------------------------------------
+
+
+
+// const makeError = () => {
+//     try {
+//        throw new Error('something went wrong')
+//     } catch (error) {
+//         console.log(error)
+//         console.warn(error)
+//         console.error(error.stack)
+//     } finally {
+//         console.log('do something')
+//     }
+// }
+// makeError()
+
+
+//---------------------------------------------------------
+
+
+
+const makeError = () => {
+    let i = 0
+    while (i <= 5) {
+        try {
+            if (i % 2 !== 0) {
+                throw new Error('Odd number')
+            }
+            console.log('Even number!')
+        } catch (error) {
+            console.error(error.stack)
+        } finally {
+            console.log('...finally')
+            i++
+        }
+    }
 }
-
-console.log(myObj.name)
-console.log(myObj.hobbies)
-myObj.hello()
-console.log(typeof myObj)
-
-const sendJSON = JSON.stringify(myObj)
-console.log(sendJSON)
-console.log(typeof sendJSON)
-
-
-const receiveJSON = JSON.parse(sendJSON)
-console.log(receiveJSON)
-console.log(typeof receiveJSON)
-console.log(receiveJSON.name)
-
-
-
+makeError()
