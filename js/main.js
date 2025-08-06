00e8b298b9dfa99b61fa0c792f092c8b4a03265f
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 14:08:29 +0000

    classes

--- js/main.js
+++ js/main.js
@@ -1,104 +1,85 @@
-const myObj = { name: 'Mike' }
-console.log(myObj)
+// class Pizza {
 
-console.log(myObj.name)
+//     constructor(pizzaSize) {
+//         this.size = pizzaSize
+//         this._crust = 'original'
+//     }
 
-const anotherObj = {
-    alive: true,
-    answer: 42,
-    hobbies: ['eat', 'sleep', 'code'],
-    beverage: {
-        morning: 'coffee',
-        afternoon: 'iced tea'
-    },
-    action: function () {
-        return `time for ${this.beverage.morning}`
+//     get pizzaCrust() {
+//         return this._crust
+//     }
+
+//     set pizzaCrust(value) {
+//         this._crust = value
+//     }
+
+//     bake() {
+//         console.log(`Making a ${this.size} ${this.type} ${this._crust} crust pizza`)
+//     }
+// }
+
+// const myPizza = new Pizza('pepperoni', 'small')
+// myPizza.bake()
+
+
+// console.log(myPizza.type)
+
+// myPizza.type = 'supreme'
+// console.log(myPizza.type)
+
+// myPizza.pizzaCrust = 'thin'
+// console.log(myPizza.type)
+
+
+// class SpecialtyPizza extends Pizza {
+//     constructor(pizzaSize) {
+//         super(pizzaSize);
+//         this.type = 'pepperoni'
+//     }
+//     slice() {
+//         console.log(`Our ${this.type} ${this.size} pizza has 8 slices`)
+//     }
+// }
+
+// const mySpecialty = new SpecialtyPizza('medium')
+// mySpecialty.slice()
+
+
+// const pizzaFactory = (pizzaSize) => {
+//     const crust = 'original'
+//     const size = pizzaSize
+
+//     return {
+//         bake: () => console.log(`Baking a ${size} ${crust} pizza`)
+//     }
+// }
+
+// const anotherPizza = pizzaFactory('small')
+// anotherPizza.bake()
+
+
+
+
+class Pizza {
+    crust = 'original'
+    #sauce = 'traditional'
+    #size
+
+    constructor(pizzaSize) {
+        this.#size = pizzaSize
+    }
+
+    getCrust() {
+        return this.crust
+    }
+
+    setCrust(value) {
+        this.crust = value
+    }
+    hereYouGo() {
+        console.log(`Here is your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza`)
     }
 }
 
-
-console.log(anotherObj)
-console.log(anotherObj.alive)
-console.log(anotherObj.answer)
-console.log(anotherObj.hobbies)
-console.log(anotherObj.hobbies[0])
-console.log(anotherObj.beverage)
-console.log(anotherObj.beverage.morning)
-console.log(anotherObj.beverage['afternoon'])
-console.log(anotherObj.action())
-
-console.log()
-console.log()
-console.log()
-
-
-const vehicle = {
-    wheels: 4,
-    engine: function () {
-        return 'vroom'
-    }
-}
-
-// inheritance
-const truck = Object.create(vehicle)
-truck.doors = 2
-console.log(truck)
-console.log(truck.wheels)
-console.log(truck.engine())
-
-const car = Object.create(vehicle)
-car.doors = 4
-car.engine = function () {
-    return 'whoosh'
-}
-console.log(car)
-console.log(car.wheels)
-console.log(car.engine())
-
-const tesla = Object.create(car)
-console.log(tesla)
-console.log(tesla.wheels)
-console.log(tesla.engine())
-tesla.engine = function () {
-    return 'shhhh'
-}
-console.log(tesla.engine())
-
-
-console.log()
-console.log()
-console.log()
-
-
-const band = {
-    vocals: 'Robert Plant',
-    guitar: 'Jimmy Page',
-    bass: 'John Paul Jones',
-    drums: 'John Bonham'
-}
-
-
-console.log(Object.keys(band))
-console.log(Object.values(band))
-
-
-for (let job in band) {
-    console.log(`On ${job}, it\'s ${band[job]}!`)
-}
-
-
-// destructuring objexts
-const { guitar: myVariable, vocals } = band
-
-console.log(band)
-console.log(myVariable)
-console.log(vocals)
-
-function sings({ vocals }) { return `${vocals} sings` }
-console.log(sings(band))
-
-
-// deleting
-delete band.drums
-
-console.log(band)
+const myPizza = new Pizza('small')
+myPizza.hereYouGo()
