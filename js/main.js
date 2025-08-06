e2b7c4cebc3b9b16ce594a90ca915c8eed019074
Michael Larbi <mickylarbi28@gmail.com>
2025-08-06 04:55:23 +0000

    scope

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
+// // can redeclare variable
+// var x = 1;
+// var x = 2;
 
-function logSomethingToConsole() {
-    console.log('something')
+// // cannot redeclare but can reassign
+// let y = 1;
+// y = 2;
+
+// // can neither reassign nor redeclare
+// const z = 3;
+
+
+
+// global scope
+var x = 1;
+let y = 2;
+const z = 3;
+
+
+// local scope (inside functions, loops, conditions or just curly brackets)
+{
+    let y = 3;
+    const z = 2;
 }
 
-function logToConsole(something) {
-    console.log(something)
+// local scope
+function myFunc() {
+    let y = 3;
+    const z = 2;
 }
-
-function addNumbers(num1, num2) {
-    return num1 + num2
-}
-
-const getUsernameFromEmail = function (email) {
-    return email.slice(0, email.indexOf('@'));
-}
-
-const toProperCase = (name) => {
-    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
-}
-
-logSomethingToConsole()
-logToConsole('logged to console')
-
-console.log(addNumbers(2, 3))
-console.log(getUsernameFromEmail('kwame@dev.start'))
-console.log(toProperCase('dAvE'))
